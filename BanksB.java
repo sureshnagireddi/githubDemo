cb3dc6a0cb96e0959da5b4efd295cbc8c19db956
Atul S Dubey <adubey22@asiapac.globalcsc.net>
2017-01-19 17:51:32 +0530

    New BAnksB

--- BanksB.java
+++ BanksB.java
@@ -3,7 +3,7 @@ public class BanksB {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("This is a bank class-b");
+System.out.println("This is a bank class-c");
 	}
 
 }
