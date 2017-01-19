d44d1bb791e4e76b7145063f689cab8570f2aaf0
Atul S Dubey <adubey22@asiapac.globalcsc.net>
2017-01-19 20:36:06 +0530

    After Branchchange-create

--- Banks.java
+++ Banks.java
@@ -4,6 +4,7 @@ public class Banks {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("This is a bank class-B");
+System.out.println("This is a bank class-Bc");
 	}
 
 }
