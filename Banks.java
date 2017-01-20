4314b923590dab621073bd1d572bcc6443a6a9c1
Atul S Dubey <adubey22@asiapac.globalcsc.net>
2017-01-20 12:55:11 +0530

    atul8 changes time 12.55pm

--- Banks.java
+++ Banks.java
@@ -4,7 +4,7 @@ public class Banks {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("This is a bank class-Bc");
-System.out.println("This is a bank class-Bc");
+System.out.println("This is a bank atul8dubey");
 	}
 
 }
